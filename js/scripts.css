3ec3e11a95de6a5daac600e82f95ecbe1e8ea9b2
HP-450G2\Admin <lameklena@gmail.com>
2017-03-27 13:53:14 +0200

    f

---
+++ js/scripts.css
@@ -0,0 +1,12 @@
+var list = document.getElementById('list');
+add = document.getElementById('addElem');
+var lista = document.getElementsByTagName('li');
+
+add.addEventListener('click', function(e) {
+	console.log(e);
+	var numer = lista.length;
+	list.innerHTML += '<li>item ' + numer + '</li>';
+});
+
+
+
